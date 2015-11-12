61999d654c332d3685eccb1a0434e2cab33ded89
jude90 <442119071@qq.com>
2015-11-12 23:31:01 +0800

    item based cf , recommend topk

--- src/main/scala/my/cf/SmallItem.scala
+++ src/main/scala/my/cf/SmallItem.scala
@@ -5,6 +5,8 @@ package my.cf
  */
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.linalg.Vectors._
+import org.apache.spark.mllib.linalg.distributed.{RowMatrix, MatrixEntry}
 
 object SmallItem {
   val sconf = new SparkConf()
@@ -13,7 +15,7 @@ object SmallItem {
     .set("spark.executor.memory", "4g")
   val sc =new SparkContext(sconf)
   def main(args: Array[String]) {
-    val small = sc.textFile("small.csv").map { line =>
+    val small = sc.textFile("data/ratings.dat").map { line =>
      val fields:Seq[String] = line.split(",")
       (fields(0).toInt, fields(1).toInt, fields(2).toDouble)
     }
@@ -23,7 +25,15 @@ object SmallItem {
     val comatrix: RDD[((Int,Int), Int)] = reidx.join(reidx).map{ case(user, (item,item2)) =>
       ((item, item2), 1)
     }.reduceByKey(_ + _)
+    val rows = sc.parallelize(Array(dense(1,2,3,4),dense(2,0,-1,0)))
+    val sim =new RowMatrix(rows).columnSimilarities()
+    val inded = sim.toIndexedRowMatrix()
+    sim.entries.foreach{ case MatrixEntry(i,j,v) =>
+       println(i,j,v)
+    }
 
+
+    println(sim.numCols(), sim.numRows())
 //    comatrix.collect()
 //    comatrix.sortBy{case ((item, item2), num) =>
 //        item
